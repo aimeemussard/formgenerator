a142fe8170516cae4b62c924182869c2300fef18
Aimée <aimee.mussard@gmail.com>
2025-05-11 20:20:05 +0200

    Add tables and update migration/model files

--- prisma/schema/migrations/20250506084550_init/migration.sql
+++ prisma/schema/migrations/20250506084550_init/migration.sql
@@ -1,9 +1,43 @@
--- CreateTable
-CREATE TABLE "Example" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+-- Form
+CREATE TABLE "form" (
+    "id" SERIAL NOT NULL,
+    "cover" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "Example_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "form_pkey" PRIMARY KEY ("id")
 );
+
+-- Responses
+CREATE TABLE "responses" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "responses_pkey" PRIMARY KEY ("id")
+);
+
+-- Questions
+CREATE TABLE "questions" (
+    "id" SERIAL NOT NULL,
+    "formId" INT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+
+    CONSTRAINT "questions_pkey" PRIMARY KEY ("id"),
+    CONSTRAINT "questions_form_fkey" FOREIGN KEY ("formId") REFERENCES "form"("id") ON DELETE CASCADE
+);
+
+-- Join table
+CREATE TABLE "questions_responses" (
+    "responsesId" INT NOT NULL,
+    "questionsId" INT NOT NULL,
+
+    PRIMARY KEY ("responsesId", "questionsId"),
+    CONSTRAINT "questions_responses_fkey" FOREIGN KEY ("responsesId") REFERENCES "responses"("id") ON DELETE CASCADE,
+    CONSTRAINT "questions_questions_fkey" FOREIGN KEY ("questionsId") REFERENCES "questions"("id") ON DELETE CASCADE
+);
+
